3aa9837e8fce1b2d4534e68647c20caba0eff503
Cpt.Tripps <Github98>
2022-06-03 15:02:08 +0200

    added Warning Message for error handling

--- src/start_gui.py
+++ src/start_gui.py
@@ -1,13 +1,14 @@
 import sys
-
 import requests
+
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
+from tkinter import StringVar, Toplevel, ttk
+from turtle import title, width
 
 from host_service import get_hosts
 from networkdevice_service import add_networkdevice, get_networkdevices
-from user_service import get_users
+from user_service import add_user, get_users
+
 
 SERVICE_TICKET = 'NC-19-7661d50f182946278a7e-nbi'
 
@@ -21,13 +22,49 @@ def show_hosts(listBox):
 def show_network_devices(listBox):
     tempList = get_networkdevices(SERVICE_TICKET)
     for i, (hostname, platformId, managementIpAddress) in enumerate(tempList, start=1):
-        listBox.insert("", "end", values=(i, hostname, platformId, managementIpAddress))
+        listBox.insert("", "end", values=(
+            i, hostname, platformId, managementIpAddress))
 
 
+#UPDATE NOT WORKING
 def show_users(listBox):
     tempList = get_users(SERVICE_TICKET)
     for i, (hostname, platformId, managementIpAddress) in enumerate(tempList, start=1):
-        listBox.insert("", "end", values=(i, hostname, platformId, managementIpAddress))
+        listBox.insert("", "end", values=(
+            i, hostname, platformId, managementIpAddress))
+
+
+def get_username(username_entry_text):
+    return username_entry_text.get()
+
+
+def get_password(password_entry_text):
+    return password_entry_text.get()
+
+
+def add_new_user():
+
+    new_window = tk.Tk()
+    new_window.title("Add new User!")
+    new_window.geometry('1200x500')
+    new_window.resizable(width=False, height=False)
+
+    username_label = ttk.Label(new_window, text="Username:")
+    username_label.pack(fill='none')
+    username_entry_text = StringVar()
+    username_entry = ttk.Entry(new_window, textvariable=username_entry_text)
+    username_entry.pack(fill='none')
+
+    password_label = ttk.Label(new_window, text="Password:")
+    password_label.pack(fill='none')
+    password_entry_text = StringVar()
+    password_entry = ttk.Entry(new_window, textvariable=password_entry_text)
+    password_entry.pack(fill='none', pady=5)
+
+    user_button_create = tk.Button(new_window, text="Create", width=15, command=(
+        lambda: add_user(SERVICE_TICKET, username_entry.get(), password_entry.get())))
+    user_button_create.pack(fill='none')
+    new_window.mainloop()
 
 
 def load_hosts(show_hosts, tab1):
@@ -53,7 +90,8 @@ def load_network_devies():
 
     tk.Button(tab2, text="Update Devices", width=15,
               command=show_network_devices(listBox)).grid(row=4, column=0)
-    tk.Button(tab2, text="Add", width=15, command=add_networkdevice).grid(row=4, column=1)
+    tk.Button(tab2, text="Add", width=15,
+              command=add_networkdevice).grid(row=4, column=1)
     tk.Button(tab2, text="Close", width=15, command=exit).grid(row=4, column=1)
 
 
@@ -67,7 +105,9 @@ def load_users():
 
     tk.Button(tab3, text="Update Users", width=15,
               command=show_users(listBox)).grid(row=4, column=0)
-    tk.Button(tab3, text="Close", width=15, command=exit).grid(row=4, column=1)
+    tk.Button(tab3, text="Add user", width=15,
+              command=add_new_user).grid(row=4, column=1)
+    tk.Button(tab3, text="Close", width=15, command=exit).grid(row=4, column=2)
 
 
 app = tk.Tk()
